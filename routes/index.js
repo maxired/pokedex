10bb5f19030674534065fcdb05b5709e2020cc51
Maxence Dalmais <github@maxired.fr>
2020-02-20 18:47:30 +0100

    init API

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,25 @@ router.get('/', function (req, res) {
     });
 });
 
+router.get('/api/', function (req, res) {
+    const { limit = 20, skip = 0 } = req.query;
+    database.getPokemons({ limit, skip }, (results) => {
+        res.send({ pokemons: results, limit, skip });
+    });
+});
+
 
 router.get('/:pokemon', function (req, res) {
     database.getPokemon(req.params.pokemon, (result) => {
         res.render('pokemon', {pokemon: result});
     });
 });
+
+router.get('/api/:pokemon', function (req, res) {
+    database.getPokemon(req.params.pokemon, (result) => {
+        res.send({ pokemon: result });
+    });
+});
+
 
 module.exports = router;
